cb7eb76e9a087bd0d0371b08f4338f49420e27ed
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-06-04 12:37:23 -0500

    logos size increased for smaller devices

--- components/TrustedBy.tsx
+++ components/TrustedBy.tsx
@@ -137,7 +137,7 @@ const TrustedBy = () => {
                         }}
                         breakpoints={{
                             299: {
-                                slidesPerView: 3,
+                                slidesPerView: 2,
                                 spaceBetween: 5,
                             },
                             499: {
@@ -164,16 +164,16 @@ const TrustedBy = () => {
                         {trusted.map((slide: TrustedBy, index: number) => (
                             <div className="swiper-slide m-0 flex items-center justify-center" key={slide._id} >
                                 <SwiperSlide key={slide._id + index} >
-                                    <div className="relative flex items-center justify-center px-6  w-[130px] h-[130px] md:w-[250px] md:h-[250px] ">
+                                    <div className="relative flex items-center justify-center px-6  w-[200px] h-[100px] md:w-[250px] md:h-[250px] ">
                                         <Image
                                             className="object-contain "
                                             src={urlFor(slide.mainImage).url()}
                                             alt={slide.name}
                                             width={1000}
                                             height={1000}
-                                            //     sizes="(max-width: 768px) 100vw,
-                                            // (max-width: 1200px) 50vw,
-                                            // 33vw"
+                                            sizes="(max-width: 768px) 100vw,
+                                            (max-width: 1200px) 50vw,
+                                            33vw"
                                             priority
                                         />
                                     </div>
