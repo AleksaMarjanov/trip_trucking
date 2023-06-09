a77bab3fea1119e0233475b571f15f2ffd83cbab
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-06-09 18:55:40 -0500

    fixed errors in Apply page so it receivs attachment, updated Image for about Hero

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,15 @@ import { TrustedBy } from '@/components'
 import Hero from '@/components/Hero'
 import React from 'react'
 
+
 const About = () => {
+
     return (
+
         <div>
             <Hero
                 title="Get to know us"
-                src="/HydroVacOnField.jpg"
+                src="/hydroVacOnField.jpg"
                 message="Our Story"
                 heading="About"
                 href="paragraph"
