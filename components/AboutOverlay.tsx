221a2e82813ee49965062edeb844990a04cc2321
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-06-13 17:41:28 -0500

    small updates on the website

--- components/AboutOverlay.tsx
+++ components/AboutOverlay.tsx
@@ -10,18 +10,18 @@ const AboutOverlay = () => {
     const [loading, setLoading] = useState(true)
 
     return (
-        <div className='min-h-screen flex flex-col items-center'>
+        <div className='flex flex-col items-center'>
             <motion.div
                 variants={staggerContainer}
                 initial="hidden"
                 whileInView="show"
                 viewport={{ once: true }}
                 className='h-[35vh] overflow-hidden' >
-                <div className="absolute z-[10] top-[30%] md:top-[45%] lg:top-[40%] left-0 w-[85%] h-full bg-white rounded-tr-[152px]" >
+                <div className="absolute z-[10] top-[30%] md:top-[45%] lg:top-[40%] left-0 w-[85%] h-[50vh] bg-white rounded-tr-[152px]" >
 
                     <motion.article
                         variants={fadeIn('up', 'tween', 0.4, 0.6)}
-                        className='relative z-[20] top-[7%] md:top-[7%] max-[325px]:left-[10%] left-[7%] md:left-[10%] 
+                        className='relative z-[20] top-[7%] md:top-[12%] max-[325px]:left-[10%] left-[7%] md:left-[10%] 
                 w-full flex flex-col-reverse sm:px-6 xl:px-44 md:space-x-24 gap-y-12 md:gap-y-0  
                 items-start justify-start md:flex-row '>
 
@@ -33,12 +33,12 @@ const AboutOverlay = () => {
                             </motion.h3>
                             <motion.p
                                 variants={textVariant(0.7)}
-                                className='leading-[30px] font-medium text-xl md:text-4xl'>
+                                className='leading-[55px] font-medium text-2xl lg:text-[38px]'>
                                 Revolutionizing Hydro Vac and Trucking Solutions from Williston, Empowering the Future of Logistics!
                             </motion.p>
 
                         </div>
-                        <div className='relative w-full md:max-w-[740px] h-[250px] md:h-[300px] lg:h-[360px]'>
+                        <div className='relative w-full  md:max-w-[740px] h-[250px] md:h-[300px] lg:h-[360px]'>
                             <Image
                                 src="/hydroVacOnField.jpg"
                                 alt="about"
@@ -66,11 +66,11 @@ w-full md:translate-x-[15%] lg:translate-x-[5%] xl:translate-x-[25%] md:scale-[1
                 initial="hidden"
                 whileInView="show"
                 viewport={{ once: true }}
-                className='mt-12 md:mt-0 md:mb-32 px-6 lg:px-40 w-full relative z-[30] top-[100%] flex flex-col md:flex-row gap-y-12 md:space-x-24 items-center justify-center '>
+                className='mt-12 md:mt-0 md:mb-32 px-6 w-full relative z-[30] top-5 md:-top-32 flex flex-col md:flex-row gap-y-12 md:space-x-24 justify-center'>
 
                 <motion.div
                     variants={fadeIn('right', 'tween', 0.5, 0.85)}
-                    className='relative w-full md:w-1/2 md:max-w-[580px] h-[600px] '>
+                    className='relative w-full sm:w-[350px] sm:h-[400px] lg:w-1/2 md:max-w-[580px] h-[500px] md:h-[700px]'>
                     <Image
                         src="/simonson.jpg"
                         alt="hydro vac"
@@ -87,7 +87,7 @@ w-full md:translate-x-[15%] lg:translate-x-[5%] xl:translate-x-[25%] md:scale-[1
                 </motion.div>
                 <motion.div
                     variants={fadeIn('left', 'tween', 0.55, 0.9)}
-                    className='w-full lg:px-6 gap-y-6 md:gap-y-12  flex md:flex-[0.5] flex-col items-start justify-center' >
+                    className='w-full px-6 gap-y-6 md:gap-y-12  flex md:flex-[0.5] flex-col items-start justify-center' >
                     <motion.h3
                         variants={textVariant(0.4)}
                         className='w-full text-3xl text-bold  text-black/75'>
@@ -95,7 +95,7 @@ w-full md:translate-x-[15%] lg:translate-x-[5%] xl:translate-x-[25%] md:scale-[1
                     </motion.h3>
                     <motion.p
                         variants={textVariant(0.6)}
-                        className='text-xl md:text-4xl font-semibold leading-[30px]'>
+                        className='leading-[55px] font-medium text-2xl lg:text-[38px]'>
                         With a strong presence in the industry, we have built a solid reputation for delivering exceptional logistics solutions to our clients. Our company operates a large fleet of well-maintained trucks and employs a team of dedicated professionals who are committed to excellence in service.
                     </motion.p>
                 </motion.div>
