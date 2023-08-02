ac2ca82e9d0a6e1f8ae3c425831ceb8437f65396
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-08-01 19:51:33 -0500

    add sitemap and robots.txt

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://trippintruckingandservices.com',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://trippintruckingandservices.com/about',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://trippintruckingandservices.com/services',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://trippintruckingandservices.com/careers',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://trippintruckingandservices.com/contact',
+            lastModified: new Date(),
+        },
+    ]
+}
