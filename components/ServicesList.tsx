2ac80ab1e20fd4637e3ffbd98dd22a9aedb44405
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-08-19 05:57:40 -0500

    add margin bottom on services

--- components/ServicesList.tsx
+++ components/ServicesList.tsx
@@ -51,13 +51,12 @@ const ServicesList = () => {
             id="services" className="dark:bg-white bg-white relative max-[475px]:mt-18 min-h-screen lg:py-12 md:px-16 z-[90] "
         >
             <motion.div
-                className='mt-12 lg:mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 px-12 text-center '>
+                className='mt-12 lg:mt-6 mb-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 px-12 text-center '>
                 {services?.map((service: Services) => (
                     <ClientSideRoute route={`/service/${service.slug.current}`} key={service._id} className=''>
                         <motion.div
                             variants={staggerContainer}
-                            initial="hidden"
-                            whileInView="show"
+                            initial="hidden" whileInView="show"
                             viewport={{ once: true }}
                             className='flex flex-col gap-y-3 md:gap-y-6'
                         >
