6b37997f880093def2d9cdba0b3a0289413ffc31
Aleksa Marjanov <98630215+AleksaMarjanov@users.noreply.github.com>
2024-11-25 13:41:12 -0600

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -30,7 +30,7 @@ const Contact = () => {
             <section className='my-6 md:my-24 items-center justify-center'>
                 <div className='flex items-center justify-center flex-col px-6 py-6'>
                     <h1 className='text-3xl md:text-5xl font-bold'>Service inquiry</h1>
-                    <p className='text-black/60 text-lg py-3 md:py-6 lg:text-xl'> Get in contact with us to see how we can assist with our expert trucking solutions</p>
+                    <p className='text-black/60 text-lg py-3 md:py-6 lg:text-xl'> Get in contact with us to see how we can assist with our expert solutions</p>
                 </div>
 
                 <div id="#contact" className='mt-12 md:mt-24 mb-6 flex flex-col lg:flex-row items-center justify-center gap-y-6 lg:gap-y-0 gap-x-12'>
