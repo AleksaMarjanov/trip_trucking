ed482da73d2894c0719689540745e4a3d5fc981a
Aleksa Marjanov <98630215+AleksaMarjanov@users.noreply.github.com>
2024-11-25 10:47:01 -0600

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,26 +1,45 @@
 import { MetadataRoute } from 'next'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-    return [
+export default function handler(req: any, res: any) {
+    const sitemap: MetadataRoute.Sitemap = [
         {
             url: 'https://trippintruckingandservices.com',
-            lastModified: new Date(),
+            lastModified: new Date().toISOString(),
         },
         {
             url: 'https://trippintruckingandservices.com/about',
-            lastModified: new Date(),
+            lastModified: new Date().toISOString(),
         },
         {
             url: 'https://trippintruckingandservices.com/services',
-            lastModified: new Date(),
+            lastModified: new Date().toISOString(),
         },
         {
             url: 'https://trippintruckingandservices.com/careers',
-            lastModified: new Date(),
+            lastModified: new Date().toISOString(),
         },
         {
             url: 'https://trippintruckingandservices.com/contact',
-            lastModified: new Date(),
+            lastModified: new Date().toISOString(),
         },
     ]
+
+    const xml = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${sitemap
+                .map(
+                    ({ url, lastModified }) => `
+                    <url>
+                        <loc>${url}</loc>
+                        <lastmod>${lastModified}</lastmod>
+                    </url>
+                `
+                )
+                .join('')}
+        </urlset>
+    `
+
+    res.setHeader('Content-Type', 'application/xml')
+    res.status(200).end(xml.trim())
 }
