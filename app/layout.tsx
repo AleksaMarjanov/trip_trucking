1817049666ea9f7126c66704cc41d0a7e5f7d2b7
Aleksa Marjanov <98630215+AleksaMarjanov@users.noreply.github.com>
2024-11-25 11:20:51 -0600

    Update layout.tsx
    Fix canonical

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,9 @@ import { Inter } from 'next/font/google'
 import { RootLayout } from '@/components'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export const metadata = {
     title: "Tripp in Trucking",
-    description: "Tripp in Trucking is a reputable trucking company specializing in hydrovac services.With their headquarters located in Williston , Tripp in Trucking has been providing exceptional trucking solutions since 2021.They offer a wide range of services including hydrovac trucking, excavation, side dumping, winching, equipment hauling.Equipped with a diverse fleet of hydrovac trucks and advanced equipment, Tripp in Trucking ensures efficient material transport and excavation operations.They prioritize safety in all aspects of their work.Committed to meeting customer needs and environmental sustainability, Tripp in Trucking implements responsible practices.For reliable and professional hydrovac and trucking services, contact Tripp in Trucking at mainoffice@trippintrucking.com.",
+    description: "Tripp in Trucking is a reputable trucking company specializing in hydrovac services. With their headquarters located in Williston, Tripp in Trucking has been providing exceptional trucking solutions since 2021. They offer a wide range of services including hydrovac trucking, excavation, side dumping, winching, equipment hauling. Equipped with a diverse fleet of hydrovac trucks and advanced equipment, Tripp in Trucking ensures efficient material transport and excavation operations. They prioritize safety in all aspects of their work. Committed to meeting customer needs and environmental sustainability, Tripp in Trucking implements responsible practices. For reliable and professional hydrovac and trucking services, contact Tripp in Trucking at mainoffice@trippintrucking.com.",
     keywords: [
         "Tripp in Trucking",
         "Hydrovac services",
@@ -21,9 +20,14 @@ export const metadata = {
         "Customer-focused",
         "Environmental sustainability",
         "Responsible practices",
-        "Contact information"
-    ]
-}
+        "Contact information",
+    ],
+    alternates: {
+        canonical: "https://trippintruckingandservices.com",
+    },
+};
+
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 
